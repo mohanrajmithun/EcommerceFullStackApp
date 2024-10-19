457e73b0f5904ff51e543f049342b9775f6830ba
Mithun <mohanmithun005@gmail.com>
2024-10-19 16:15:12 -0500

    pushing to remote

--- Backend/SalesInvoiceGeneratorServiceAPI/SalesInvoiceGeneratorServiceAPI/obj/Debug/net6.0/SalesInvoiceGeneratorServiceAPI.AssemblyInfo.cs
+++ Backend/SalesInvoiceGeneratorServiceAPI/SalesInvoiceGeneratorServiceAPI/obj/Debug/net6.0/SalesInvoiceGeneratorServiceAPI.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalesInvoiceGeneratorServiceAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3444ba82e1b19d5707efb5efea5bc7a152793b82")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81588598a8285c60d99f86fec37adbf7909ca0bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalesInvoiceGeneratorServiceAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalesInvoiceGeneratorServiceAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
